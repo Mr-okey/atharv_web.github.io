989f31d27b520d2b0fe2acc0d0ea8edff96a9e8f
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2023-01-11 18:38:34 +0530

    added 7segment clock

--- projects/7segment/index.js
+++ projects/7segment/index.js
@@ -1,11 +1,5 @@
-const bars = document.getElementsByClassName("bar")
-const num1 = document.getElementsByClassName("num1")
-const num2 = document.getElementsByClassName("num2")
-const num3 = document.getElementsByClassName("num3")
-
 function dispNum(position, num, color) {
-    console.log(num)
-    
+
     let topBar = position[0].childNodes[1]
     let first_two_1 = position[0].childNodes[3].childNodes[1]
     let first_two_2 = position[0].childNodes[3].childNodes[3]
@@ -85,51 +79,99 @@ function dispNum(position, num, color) {
             bottomBar.style.background = color
             break;
         default:
-            topBar.style.background = "white"
-            first_two_1.style.background = "white"
-            first_two_2.style.background = "white"
-            midBar.style.background = "white"
-            second_two_1.style.background = "white"
-            second_two_2.style.background = "white"
-            bottomBar.style.background = "white"
+            topBar.style.background = "transparent"
+            first_two_1.style.background = "transparent"
+            first_two_2.style.background = "transparent"
+            midBar.style.background = "transparent"
+            second_two_1.style.background = "transparent"
+            second_two_2.style.background = "transparent"
+            bottomBar.style.background = "transparent"
             break;
 
     }
 
 }
 
-function show() {
+
+// analogue functioning
+
+const bars = document.getElementsByClassName("bar")
+const num1 = document.getElementsByClassName("num1")
+const num2 = document.getElementsByClassName("num2")
+const num3 = document.getElementsByClassName("num3")
+const select_color_text = document.getElementById("select-color-text")
+
+// refresh twice every second
+setInterval(() => {
+
     // reset grid
     dispNum(num1, -1)
     dispNum(num2, -1)
     dispNum(num3, -1)
-
-    const color = document.getElementById("color").value
+    let color = document.getElementById("color").value
     const integer = document.getElementById("number").value
     let ones = 0
     let tens = 0
     let hund = 0
-    
-    if(integer > 1000) {
+    select_color_text.style.color = color
+    if (integer >= 1000) {
         alert("number should be below 1000")
+        document.getElementById("number").value = 0
         return
     }
-    if(integer >= 100) {
-        hund = Math.floor(integer/100)
-        tens = Math.floor((integer - (hund*100))/10)
-        ones = Math.floor((integer - (hund*100)) - (tens*10))
+    if (integer >= 100) {
+        hund = Math.floor(integer / 100)
+        tens = Math.floor((integer - (hund * 100)) / 10)
+        ones = Math.floor((integer - (hund * 100)) - (tens * 10))
         dispNum(num1, hund, color)
         dispNum(num2, tens, color)
         dispNum(num3, ones, color)
     }
     else if (integer <= 99) {
-        tens = Math.floor(integer/10)
-        ones = Math.floor((integer - (tens*10)))
-        dispNum(num1, tens, color)
-        dispNum(num2, ones, color)
-    } else if(integer <= 9) {
+        tens = Math.floor(integer / 10)
+        ones = Math.floor((integer - (tens * 10)))
+        dispNum(num2, tens, color)
+        dispNum(num3, ones, color)
+    } else if (integer <= 9) {
         ones = integer
-        dispNum(num1, ones, color)
+        dispNum(num3, ones, color)
     }
+}, 500)
+
+
+
+// clock functioning
+const Hour_tens = document.getElementsByClassName("Hour-tens")
+const Hour_ones = document.getElementsByClassName("Hour-ones")
+const mins_tens = document.getElementsByClassName("mins-tens")
+const mins_ones = document.getElementsByClassName("mins-ones")
+const sec_tens = document.getElementsByClassName("sec-tens")
+const sec_ones = document.getElementsByClassName("sec-ones")
+
+setInterval(() => {
+    let d = new Date();
+    let hh = d.getHours()
+    let mm = d.getMinutes()
+    let ss = d.getSeconds()
+    color = document.getElementById("color").value
     
-}
+    // reset
+    dispNum(Hour_tens, -1)
+    dispNum(Hour_ones, -1)
+    dispNum(mins_tens, -1)
+    dispNum(mins_ones, -1)
+    dispNum(sec_tens, -1)
+    dispNum(sec_ones, -1)
+
+    // set
+    dispNum(Hour_tens, Math.floor(hh / 10), color)
+    dispNum(Hour_ones, (hh - ((Math.floor(hh / 10)) * 10)),color)
+    dispNum(mins_tens, Math.floor(mm / 10), color)
+    dispNum(mins_ones, (mm - ((Math.floor(mm / 10)) * 10)), color)
+    dispNum(sec_tens, Math.floor(ss / 10), color)
+    dispNum(sec_ones, (ss - ((Math.floor(ss / 10)) * 10)), color)
+
+    delete d
+}, 1000)
+
+
