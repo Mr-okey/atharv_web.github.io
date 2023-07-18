4c78790c09db002485195d64fd412d812423112e
Atharv Vyas <95745167+1447bits@users.noreply.github.com>
2023-07-18 21:02:36 +0530

    added animated sticks comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import RotatingSticks from './Components/RotatingSticks';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      {/* <div id='animatedSticks'>
+      <RotatingSticks />
+      </div> */}
+
     </div>
   );
 }
