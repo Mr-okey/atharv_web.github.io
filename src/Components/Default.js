1241a1ed61d7acf8b8df6014ccc1d1d453418800
Atharv Vyas <95745167+1447bits@users.noreply.github.com>
2023-07-28 18:49:00 +0530

    +HomePg

--- src/Components/Default.js
+++ src/Components/Default.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBar from './NavBar'
+import "../App.css"
 
 export default function DefaultComp(props) {
   return (
-    <div>
+    <div id='defaultPg'>
+      <div id='defpg-child-pg'>
         {props.children}
+      </div>
+      <div id='default-navbar'>
         <NavBar />
+      </div>
     </div>
   )
 }
--- src/Pages/HomePg.js
+++ src/Pages/HomePg.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "../styles/Homepg.css"
 
 export default function HomePg() {
     return (
         <div id='HomePg'>
-            HomePg
+            <div id="HomePg-centerBox">
+                <p>Hi. My Self Atharv vyas</p>
+                <strong>Web Developer</strong>
+                <p>Inspirer of Imagination</p>
+            </div>
         </div>
     )
 }
