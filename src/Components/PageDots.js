dd72bc43f78364ae65f4280eb240ea48b122dc28
Atharv Vyas <95745167+1447bits@users.noreply.github.com>
2023-08-02 23:49:50 +0530

    work navdots

--- src/Components/PageDots.js
+++ src/Components/PageDots.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import "../styles/PageDots.css"
 import { Link } from 'react-router-dom'
+import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
 
 export default function PageDots() {
+  const handleLinkClick = (targetid) => {
+    const bodyCardsElement = document.getElementById(targetid);
+    if (bodyCardsElement) {
+      bodyCardsElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div id='PageDots'>
-        <Link className='pagedot' to="#HomePg"></Link>           
-        <Link className='pagedot' to="#HomePg"></Link>           
-        <Link className='pagedot' to="#ProjectPg"></Link>           
+      <Link className='pagedot' onClick={() => handleLinkClick("HomePg")} to="#HomePg"></Link>
+      <Link className='pagedot' onClick={() => handleLinkClick("SkillPg")} to="#SkillPg"></Link>
+      <Link className='pagedot' onClick={() => handleLinkClick("ProjectPg")} to="#ProjectPg"></Link>
     </div>
   )
 }
