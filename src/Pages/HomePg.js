b408966d2058a573b9fce61b0e40047d5a1a5a42
1447bits <untilinfinity00@gmail.com>
2023-08-05 23:54:51 +0530

    added rotating sticks

--- src/Pages/HomePg.js
+++ src/Pages/HomePg.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "../styles/Homepg.css"
+import RotatingSticks from '../Components/RotatingSticks'
 
 export default function HomePg() {
     return (
         <div id='HomePg'>
+            <RotatingSticks />
             <div id="HomePg-centerBox">
                 <p>Hi. My Self Atharv vyas</p>
                 <strong>Web Developer</strong>
