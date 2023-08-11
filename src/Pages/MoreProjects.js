d4ad1f81c9b34593ee930479f4a9694a17d0e659
1447bits <untilinfinity00@gmail.com>
2023-08-11 21:40:50 +0530

    minor changes

--- src/Pages/MoreProjects.js
+++ src/Pages/MoreProjects.js
@@ -35,7 +35,7 @@ export default function MoreProjects(props) {
         },
         {
             img: cardDesignSS,
-            heading: "card centric webpage front-end",
+            heading: "card centric design"
             id: 3,
             status: "compleated",
             desc: "A card centric design webpage frontend, responsive, made with vanila html/css and javascript",
