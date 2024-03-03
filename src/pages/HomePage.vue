b6c4e211cb3e987ba2b30d40f2e062c1937331a6
1447bits <untilinfinity00@gmail.com>
2024-03-03 19:13:43 +0530

    init vue application - fresh [03/03/2024]

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "Home-page",
+    data() {
+        return {
+            helloMessage: "Welcome to 1447bits portfollio"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ helloMessage }}</h1>
+</template>
